3db788261914b9dc18e0d9b7f54e70bb4f45750d
jf18j492 <jonas.frey@csh.unibe.ch>
2022-01-16 04:14:03 +0100

    clean start with helpers , see ./create_new_folder_project.js

--- dependencies_lesson/dependencies_lesson.kt
+++
@@ -1,6 +0,0 @@
-// file App.kt
-package com.my.stuff
-
-fun main(args: Array<String>) {
-  println("main in dependencies_lesson.kt was called");
-}
--- dependencies_lesson/package_a.kt
+++
@@ -1,9 +0,0 @@
-package com.my.stuff.package_a
-
-fun fun_from_package_a(){
-    println("asdf")
-}
-
-// fun main() {  
-//   println("main in package_a.kt was called");
-// }
